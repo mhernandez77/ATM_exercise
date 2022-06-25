a4fd4991cae549f361676aabaca162c30615c26d
Maleni A Hernandez <maleni.alexis@gmail.com>
2022-06-25 17:04:10 -0500

    button added

--- starterCode.jsx
+++ starterCode.jsx
@@ -1,8 +1,10 @@
 
-const ATMDeposit = (onChange) => {
+const ATMDeposit = (onChange, isDeposit) => {
+    const choice = ["Deposit", "Cash Back"];
     return (
         <label className="label huge">
-            Deposit:
+            {/*Deposit:*/}
+            <h3>{choice[Number(!isDeposit)]}</h3>
             <input type="number" onChange={onChange}></input>
             <input type="submit" value={"submit"} onClick={handleSubmit}></input>
             {/*<h2 id="total">{status}</h2>*/}
@@ -16,13 +18,15 @@ const Account = () => {
     console.log(`handleChange ${event.target.value}`);
     setAccountState(event.target.value);
   };
-  const handleSubmit = event => {
-    alert(`Account total = ${accountState}`);
-    event.preventDefault();
-  };
+  // const handleSubmit = event => {
+  //   alert(`Account total = ${accountState}`);
+  //   event.preventDefault();
+  // };
 
     let transactionState = 0; // state of this transaction
     const [totalState, setTotalState] = React.useState(0);
+    const [isDeposit, setIsDeposit] = React.useState(true);
+
     let status = `Account Balance $ ${totalState}`;
     //print for checking if rerendering correctly
     console.log('Account rendered')
@@ -31,9 +35,10 @@ const Account = () => {
         transactionState = Number(event.target.value);
     };
     const handleSubmit =  () => {
+        let newTotal = isDeposit ? totalState + setIsDeposit : totalState - deposit;
         setTotalState(totalState + transactionState);
-        // document.getElementById("total").innerHTML = status;
         event.preventDefault();
+        // document.getElementById("total").innerHTML = status;
     };
 //     return (
 //         <label className="label huge">
@@ -48,7 +53,10 @@ const Account = () => {
 return (
     <form onSubmit={handleSubmit}>
       <h2 id={'total'}>Account Balance {accountState}</h2>
-      <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
+       <button onClick={()=> setIsDeposit(true)}>Deposit</button>
+        <h2 id={'total'}>Account Balance {accountState}</h2>
+        <button onClick={()=> setIsDeposit(false)}>Cash Back</button>
+        <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
     </form>
   );
 };
